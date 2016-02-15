49a04938f32f8b22f11208354239d47565c42b54
Daniel Romero <romerodaniel.k90@gmail.com>
2016-02-14 22:12:41 -0800

    double slideouts from slideout js

---
+++ README.md
@@ -0,0 +1,5 @@
+# Double Slideout Sidebars from [Slideout JS](https://github.com/Mango/slideout)
+
+> Comments give pointers on how to get this done
+
+There is a better way to do this but this works for me at the moment. IF you have a better way let me know or leave a link to your project on the issues tab
---
+++ js/slideouts.js
@@ -0,0 +1,46 @@
+(function () {
+  
+  // declare slideouts as new variables
+  var leftSlideout = new  Slideout({
+    'panel': document.getElementById('panel'),
+    'menu': document.getElementById('left-menu'),
+    'padding': 256,
+    'tolerance': 70
+  });
+  var rightSlideout = new  Slideout({
+    'panel': document.getElementById('panel'),
+    'menu': document.getElementById('right-menu'),
+    'padding': 256,
+    'tolerance': 70,
+    'side': 'right'
+  });
+
+
+  var leftMenu = document.getElementById('left-menu');
+  var rightMenu = document.getElementById('right-menu');
+
+  document
+    .querySelector('#toggle-left')
+    .addEventListener('click', function() {
+      leftSlideout.toggle();
+      leftMenu.style.zIndex = '0';
+
+      leftSlideout.on('close', function () {
+        lefttMenu.style.zIndex = '-1';
+      });
+
+  });
+
+  document
+    .querySelector('#toggle-right')
+    .addEventListener('click', function() {
+      rightSlideout.toggle();
+      rightMenu.style.zIndex = '0';
+
+      rightSlideout.on('close', function () {
+        rightMenu.style.zIndex = '-1';
+      });
+
+  });
+
+})();
